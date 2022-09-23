d5db7cbaf30ee9fb291a58a6eb135e0cff01d764
hello <hello>
2022-09-23 17:38:28 +0900

    029과제 개선 -. 영화관정보 지도 표시

--- TrendMedia_Assignment/Theater/TheaterViewController.swift
+++ TrendMedia_Assignment/Theater/TheaterViewController.swift
@@ -12,8 +12,8 @@ import MapKit
 
 
 /*질문
- -.영화관 구조체 데이터에 어떻게 접근하도록 만드는지? 일일이 함수 작성하는 방식은 당연히 아닐 것 같은데 구조체에서 값을 접근하지 못하겠음
- -.개별적으로 함수만든다고 쳐도 removeAnnotation 매개변수가 MKAnnotation타입인데 MKAnnotation을 넣을수가 없음
+ -.영화관 구조체 데이터에 어떻게 접근하도록 만드는지? 일일이 함수 작성하는 방식은 당연히 아닐 것 같은데 구조체에서 값을 접근하지 못하겠음 -> 해결: 어떤 매개변수가 필요한지 확인하여 함수를 만들고 매개변수를 반복문으로 처리할 수 있는 함수를 하나더 만들어 처리(함수 내 함수 형태)
+ -.removeAnnotation메서드에 넣을 매개변수를 어떻게 조건문을 통해 넣을 수 있는지? 배열형태로 하면 어떤내용인지 모르고 순서로만 확인하는 문제 발생
  */
 
 /*지도 포인트
@@ -32,21 +32,24 @@ class TheaterViewController: UIViewController {
     let locationManager = CLLocationManager()
     let theaterInfo = TheaterList()
     
+    var theaterType = Array<String>()
+    var theaterName = Array<String>()
+    var theaterCoordinate = Array<CLLocationCoordinate2D>()
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        centerRegion()
         
-        setRegionAndAnnotation()
-        setRegionAndAnnotation2()
-        setRegionAndAnnotation3()
+        
+        
+        centerRegion()
+        showTheaters()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         showTheaterList()
-        
     }
     
     func centerRegion() {
@@ -55,47 +58,52 @@ class TheaterViewController: UIViewController {
         mapView.setRegion(region, animated: true)
     }
     
-    func setRegionAndAnnotation() {
-        
-        for data in
+    func showTheaters() {
+        for i in 0..<theaterInfo.mapAnnotations.count {
+            let branch = theaterInfo.mapAnnotations[i].location
+            let latitude = theaterInfo.mapAnnotations[i].latitude
+            let longitude = theaterInfo.mapAnnotations[i].longitude
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            setAnnotation(branch: branch, coordinate: coordinate)
+        }
+    }
+    
+    func setAnnotation(branch: String, coordinate: CLLocationCoordinate2D) {
         let annotation = MKPointAnnotation()
-        let center = CLLocationCoordinate2D(latitude: latitude[0], longitude: longitude[0])
-        annotation.coordinate = center
-        annotation.title = location[0]
+        annotation.coordinate = coordinate
+        annotation.title = branch
         mapView.addAnnotation(annotation)
     }
     
-    func setRegionAndAnnotation2() {
-        let location = theaterInfo.mapAnnotations.filter { $0.type == "메가박스" }.map { $0.location }
-        let latitude = theaterInfo.mapAnnotations.filter { $0.type == "메가박스" }.map { $0.latitude }
-        let longitude = theaterInfo.mapAnnotations.filter { $0.type == "메가박스" }.map { $0.longitude }
-
-        let annotation = MKPointAnnotation()
-        let center = CLLocationCoordinate2D(latitude: latitude[1], longitude: longitude[1])
+    //잘못된 접근방식
+    func setRegionAndAnnotation(name: String, location: CLLocationCoordinate2D) {
+        for i in 0..<theaterInfo.mapAnnotations.count {
+            let type = theaterInfo.mapAnnotations[i].type
+            let loacation = theaterInfo.mapAnnotations[i].location
+            let longitude = theaterInfo.mapAnnotations[i].longitude
+            let latitude = theaterInfo.mapAnnotations[i].latitude
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            
+            self.theaterType.append(type)
+            self.theaterName.append(loacation)
+            self.theaterCoordinate.append(coordinate)
+        }
+        print(theaterType)
+        print(theaterName)
+        print(theaterCoordinate)
         
-
-        annotation.coordinate = center
-        annotation.title = location[1]
-        mapView.addAnnotation(annotation)
-    }
-    
-    func setRegionAndAnnotation3() {
-        let location = theaterInfo.mapAnnotations.filter { $0.type == "CGV" }.map { $0.location }
-        let latitude = theaterInfo.mapAnnotations.filter { $0.type == "CGV" }.map { $0.latitude }
-        let longitude = theaterInfo.mapAnnotations.filter { $0.type == "CGV" }.map { $0.longitude }
-
         let annotation = MKPointAnnotation()
-        let center = CLLocationCoordinate2D(latitude: latitude[0], longitude: longitude[0])
-
-        annotation.coordinate = center
-        annotation.title = location[0]
+        annotation.coordinate = location
+        annotation.title = name
         mapView.addAnnotation(annotation)
     }
     
     func showTheaterList() {
+        let allAnnotations = self.mapView.annotations
+        
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let megabox = UIAlertAction(title: "메가박스", style: .default) { _ in
-            //mapView.removeAnnotation()
+            //mapView.removeAnnotation(<#T##annotation: MKAnnotation##MKAnnotation#>)
         }
         let lotte = UIAlertAction(title: "롯데시네마", style: .default) { _ in
             //mapView.removeAnnotation()
@@ -104,9 +112,9 @@ class TheaterViewController: UIViewController {
             //mapView.removeAnnotation()
         }
         let allTheater = UIAlertAction(title: "전체보기", style: .default) { _ in
-            
+            self.mapView.removeAnnotation(allAnnotations[0])
         }
-                                  
+        
         let cancel = UIAlertAction(title: "취소", style: .cancel)
         actionSheet.addAction(megabox)
         actionSheet.addAction(lotte)
@@ -115,5 +123,4 @@ class TheaterViewController: UIViewController {
         actionSheet.addAction(cancel)
         present(actionSheet, animated: true)
     }
-
 }
