4dfadfa32188fdb8bc3b9e038f736860a102339f
hello <hello>
2022-10-02 15:51:05 +0900

    035과제 -.Realm사용하여 쇼핑목록 추가/삭제 구현 -.탭제스처 사용하여 키보드 내리기 구현

---
+++ TrendMedia_Assignment/Realm/RealmModel.swift
@@ -0,0 +1,27 @@
+//
+//  RealmModel.swift
+//  TrendMedia_Assignment
+//
+//  Created by Mac Pro 15 on 2022/10/02.
+//
+
+import Foundation
+import RealmSwift
+
+class ShoppingList: Object {
+    //1.테이블 만들기(컬럼)
+    @Persisted var checkbox: Bool?
+    @Persisted var shoppingContents: String
+    @Persisted var favorite: Bool?
+    
+    //2.PK등록
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    //3.초기화
+    convenience init(checkbox: Bool?, shoppingContents: String, favorite: Bool?) {
+        self.init()
+        self.checkbox = false
+        self.shoppingContents = shoppingContents
+        self.favorite = false
+    }
+}
--- TrendMedia_Assignment/Shopping/ShoppingTableViewController.swift
+++ TrendMedia_Assignment/Shopping/ShoppingTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 /*쇼핑목록리스트 포인트
  -. 테이블뷰 row끼리 간격을 줄 수 없으니 view위에 객체를 얹어서 간격이 있는 것처럼 보이게 만들어 준다.
@@ -25,16 +26,43 @@ class ShoppingTableViewController: UITableViewController {
     @IBOutlet weak var shoppingListTextField: UITextField!
     @IBOutlet weak var shoppingListAddButton: UIButton!
     
-    var shoppingList = ["그립톡 구매하기", "사이다 구매", "아이폰 최저가 알아보기", "양말"]
+    //var shoppingList = ["그립톡 구매하기", "사이다 구매", "아이폰 최저가 알아보기", "양말"]
+    
+    //1. Realm파일 저장경로 설정
+    let localRealm = try! Realm()
+    
+    var shoppingListArray : Results<ShoppingList>!
+    var dummy = Array<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        hideKeyboard()
         navigationItem.title = "쇼핑"
         
         headerViewAttribute()
         shoppingListTextFieldAttribute()
         shoppingListAddButtonAttribute()
+        shoppingListAddButton.addTarget(self, action: #selector(shoppingListAddButtonClicked), for: .touchUpInside) //추가버튼 클릭시 쇼핑목록 생성 구현
+        
+        print("Realm is located at:", localRealm.configuration.fileURL!)
+        
+        shoppingListArray = localRealm.objects(ShoppingList.self).sorted(byKeyPath: "shoppingContents", ascending: true)
+        print(shoppingListArray)
+    }
+    
+    @objc func shoppingListAddButtonClicked() {
+        //        shoppingList.append(shoppingListTextField.text!)
+        
+        //Realm파일 테이블 레코드 생성
+        let shoppingList = ShoppingList(checkbox: false, shoppingContents: shoppingListTextField.text!, favorite: false)
+        
+        //Realm파일 테이블 레코드 추가
+        try! localRealm.write {
+            localRealm.add(shoppingList)
+            print("Realm Success")
+            tableView.reloadData() //
+        }
     }
     
     func headerViewAttribute() {
@@ -60,14 +88,17 @@ class ShoppingTableViewController: UITableViewController {
     //row 생성
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return shoppingList.count
+        return shoppingListArray.count
     }
     
     //swipe제거 기능 추가
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            shoppingList.remove(at: indexPath.row)
-            tableView.reloadData()
+            try! localRealm.write {
+                localRealm.delete(shoppingListArray[indexPath.row]) //indexPath에 맞는 Realm레코드 삭제
+                print("Realm Deleted")
+                tableView.reloadData()
+            }
         }
     }
     
@@ -78,7 +109,7 @@ class ShoppingTableViewController: UITableViewController {
         cell.cellView.layer.cornerRadius = 10
         cell.checkBoxButton.image = UIImage(systemName: "checkmark.square")
         cell.checkBoxButton.tintColor = .black
-        cell.buyList.text = shoppingList[indexPath.row]
+        cell.buyList.text = shoppingListArray[indexPath.row].shoppingContents
         cell.buyList.backgroundColor = .clear
         cell.buyList.font = .systemFont(ofSize: 15)
         cell.starButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
@@ -89,7 +120,25 @@ class ShoppingTableViewController: UITableViewController {
     
     //텍스트필드값 tableViewCell 표시
     @IBAction func shoppingListTextFieldReturn(_ sender: UITextField) {
-        shoppingList.append(shoppingListTextField.text!)
-        tableView.reloadData()
+        let shoppingList2 = ShoppingList(checkbox: false, shoppingContents: shoppingListTextField.text!, favorite: false)
+        
+        //Realm파일 테이블 레코드 추가
+        try! localRealm.write {
+            localRealm.add(shoppingList2)
+            print("Realm Success")
+            tableView.reloadData() //
+        }
+    }
+}
+
+extension ShoppingTableViewController {
+    //키보드 내리기 탭제스처 구현
+    func hideKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dissmissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dissmissKeyboard() {
+        view.endEditing(true)
     }
 }
